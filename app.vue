f318fab7deebba01daaa02759e48fb3f27c59e63
Bruno De Rosa <brunoderosa180@gmail.com>
2023-04-01 10:02:43 -0300

    fix mail

--- app.vue
+++ app.vue
@@ -75,57 +75,59 @@ console.log(works.value[0].images)
         Bruno De Rosa's Portfolio
       </Title>
     </Head>
-    <section id="about" class="flex p-30 justify-center">
-      <div class="flex-col scale-150  flex justify-center items-center">
-        <h1 class="text-xl scale-200">Hi! I'm Bruno</h1>
-        <!-- <h3 class="text-base scale-150 ">Web Developer</h3> -->
-        <p class="text-center word-wrap px-10">
-          I'm a web developer based in Argentina. I love building apps that
-          solve people's problems.
-        </p>
-        <div class="justify-center flex gap-5">
-          <a href="https://www.linkedin.com/in/bruno-de-rosa-943a83246/"
-            ><div
-              class="i-mdi:linkedin scale-200 bg-purple-200 hover:bg-green-200 hover:scale-210 hover:transition shadow"
-            ></div
-          ></a>
-          <a href="https://github.com/brunoderosa1"
-            ><div
-              class="i-mdi:github scale-200 bg-purple-200 hover:bg-green-200 hover:scale-210 hover:transition shadow"
-            ></div
-          ></a>
+    <Body class="flex flex-col gap-20 content-center">
+      <section id="about" class="flex p-30 pb-10 justify-center">
+        <div class="flex-col scale-150  flex justify-center items-center">
+          <h1 class="text-xl scale-200">Hi! I'm Bruno</h1>
+          <!-- <h3 class="text-base scale-150 ">Web Developer</h3> -->
+          <p class="text-center word-wrap px-10">
+            I'm a web developer based in Argentina. I love building apps that
+            solve people's problems.
+          </p>
+          <div class="justify-center flex gap-5">
+            <a href="https://www.linkedin.com/in/bruno-de-rosa-943a83246/"
+              ><div
+                class="i-mdi:linkedin scale-200 bg-purple-200 hover:bg-green-200 hover:scale-210 hover:transition shadow"
+              ></div
+            ></a>
+            <a href="https://github.com/brunoderosa1"
+              ><div
+                class="i-mdi:github scale-200 bg-purple-200 hover:bg-green-200 hover:scale-210 hover:transition shadow"
+              ></div
+            ></a>
+          </div>
+          <p class="font-bold">Checkout my latest work!</p>
         </div>
-        <p class="font-bold">Checkout my latest work!</p>
-      </div>
-    </section>
-    <section class="mt-10" id="work">
-      <div
-        v-for="(work, index) in works"
-        :key="index"
-        class="flex flex-row justify-center"
-      >
-        <va-carousel
-          class="w-1/4"
-          v-model="value"
-          :items="work.images"
-          :indicators="true"
-          :swipable="true"
-        />
-        <div class="flex flex-col items-center justify-center p-5 w-100">
-          <h1 class="text-center">{{ work.title }}</h1>
-          <p class="w-100 text-center">{{ work.description }}</p>
-          <div class="flex flex-row mt-5 gap-5">
-            <div
-              v-for="item in work.stack"
-              :class="`${item.icon} scale-200`"
-            ></div>
+      </section>
+      <section class="mt-5" id="work">
+        <div
+          v-for="(work, index) in works"
+          :key="index"
+          class="flex flex-row justify-center"
+        >
+          <va-carousel
+            class="w-1/4"
+            v-model="value"
+            :items="work.images"
+            :indicators="true"
+            :swipable="true"
+          />
+          <div class="flex flex-col items-center justify-center p-5 w-100">
+            <h1 class="text-center">{{ work.title }}</h1>
+            <p class="w-100 text-center">{{ work.description }}</p>
+            <div class="flex flex-row mt-5 gap-5">
+              <div
+                v-for="item in work.stack"
+                :class="`${item.icon} scale-200`"
+              ></div>
+            </div>
           </div>
         </div>
-      </div>
-    </section>
-    <section id="contact" class="flex items-center gap-1 justify-center">
-      <p class="text-center mt-15">You can contact me at</p><p class="text-center mt-15"><a class="no-underline color-purple-200 hover:color-purple-600 hover:bg-green-200 hover:scale-110 hover:transition" :href="'mailto:brunoderosa180@email.com'" >brunoderosa180@email.com</a>.</p>
-    </section>
+      </section>
+      <section id="contact" class="flex items-center gap-1 justify-center">
+        <p class="text-center mt-15">You can contact me at</p><p class="text-center mt-15"><a class="no-underline color-purple-200 hover:color-purple-600 hover:bg-green-200 hover:scale-110 hover:transition" :href="'mailto:brunoderosa180@email.com'" >brunoderosa180@gmail.com</a>.</p>
+      </section>
+    </Body>
   </div>
 </template>
 
